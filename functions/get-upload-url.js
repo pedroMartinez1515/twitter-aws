3353f7990228251cbcfe9f65ed977f1633a3b2d5
pedro martinez <mulinhazikoli@gmail.com>
2023-07-03 15:37:08 -0300

    add unit test for getImageUploadUrl query

--- functions/get-upload-url.js
+++ functions/get-upload-url.js
@@ -1,34 +1,33 @@
-const S3 = require('aws-sdk/clients/s3');
-const s3 = new S3({ useAccelerateEndpoint: true });
+const S3 = require('aws-sdk/clients/s3')
+const s3 = new S3({ useAccelerateEndpoint: true })
 const ulid = require('ulid')
 
-const {BUCKET_NAME} = process.env 
+const { BUCKET_NAME } = process.env
 
-module.exports.handler = async(event) =>{
-    const id = ulid.ulid()
-    const key = `${event.identity.username}/${id}`
+module.exports.handler = async (event) => {
+  const id = ulid.ulid()
+  let key = `${event.identity.username}/${id}`
 
-    const extension = event.arguments.extension
-    if(extension){
-        if(extension.startsWith('.')){
-            key += extension
-        }else{
-            key+= `${extension}`
-        }
+  const extension = event.arguments.extension
+  if (extension) {
+    if (extension.startsWith('.')) {
+      key += extension
+    } else {
+      key += `.${extension}`
     }
+  }
 
-    const contentType = event.arguments.contentType || 'image/jpeg'
-    if(!contentType.startsWith('image/')){
-        throw new Error('contet type should be an image')
-    }
+  const contentType = event.arguments.contentType || 'image/jpeg'
+  if (!contentType.startsWith('image/')) {
+    throw new Error('content type should be an image')
+  }
 
-    const params = {
-        Bucket: BUCKET_NAME,
-        Key: key,
-        ACL: 'public-read',
-        ContentType: contentType
-    }
-    const signedUrl = s3.getSignedUrl('putObject', params);
-
-    return signedUrl
+  const params = {
+    Bucket: BUCKET_NAME,
+    Key: key,
+    ACL: 'public-read',
+    ContentType: contentType
+  }
+  const signedUrl = s3.getSignedUrl('putObject', params)
+  return signedUrl
 }
